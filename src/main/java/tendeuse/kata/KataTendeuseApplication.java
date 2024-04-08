5e86eb8b3ececfe138088cd446cd8f7ab1651ae2
zoubairGuesmi <zoubair.guesmi92@gmail.com>
2024-04-08 15:32:23 +0200

    Initial commit

---
+++ src/main/java/tendeuse/kata/KataTendeuseApplication.java
@@ -0,0 +1,13 @@
+package tendeuse.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataTendeuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataTendeuseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tendeuse/kata/KataTendeuseApplicationTests.java
@@ -0,0 +1,13 @@
+package tendeuse.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataTendeuseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
